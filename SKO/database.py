979705ee2b5e4d05a7fedf6fa9b01205a40dbb60
Kaze Wong <kazewong.physics@gmail.com>
2024-09-01 10:14:00 -0400

    Update postgres from ls is ready

--- SKO/database.py
+++ SKO/database.py
@@ -2,39 +2,43 @@ from flask import Blueprint, request, jsonify, current_app
 from minio import Minio
 from label_studio_sdk.client import LabelStudio
 import psycopg
-import ffmpeg
-import uuid
 
-def connect_database() -> psycopg:
+
+def connect_postgres() -> psycopg:
     database_client = psycopg.connect(
         host=current_app.config["POSTGRES_HOST"],
         port=current_app.config["POSTGRES_PORT"],
         user=current_app.config["POSTGRES_USER"],
         password=current_app.config["POSTGRES_PASSWORD"],
-        dbname=current_app.config["POSTGRES_DB"]
+        dbname=current_app.config["POSTGRES_DB"],
     )
     return database_client
+
 
 def connect_minio() -> Minio:
     minio_client = Minio(
         endpoint=current_app.config["MINIO_ENDPOINT"],
         access_key=current_app.config["MINIO_ACCESS_KEY"],
-        secret_key=current_app.config["MINIO_SECRET_KEY"]
+        secret_key=current_app.config["MINIO_SECRET_KEY"],
     )
     return minio_client
+
 
 def connect_label_studio() -> LabelStudio:
     label_studio_client = LabelStudio(
         base_url=current_app.config["LABEL_STUDIO_URL"],
-        api_key=current_app.config["LABEL_STUDIO_API_KEY"]
+        api_key=current_app.config["LABEL_STUDIO_API_KEY"],
     )
     return label_studio_client
 
+
 database_service = Blueprint("database", __name__)
+
 
 @database_service.route("/test", methods=["GET"])
 def test():
-    db = connect_database()
+    db = connect_postgres()
     minio = connect_minio()
     label_studio = connect_label_studio()
     return jsonify({"message": "Success"})
+
--- SKO/webhook.py
+++ SKO/webhook.py
@@ -1,26 +1,67 @@
 from flask import Blueprint, request, jsonify, current_app
+from .database import connect_postgres
+import uuid
 
 webhook_service = Blueprint("webhook", __name__)
+
 
 @webhook_service.route("/annotate_image", methods=["POST"])
 def handle_annotate_image():
     print("Received POST request")
-    # if request.method == "POST":
-    #     data = request.json
-    #     print(data)
-    #     for annotation in data["annotations"]['result']:
-    #         if annotation['type'] == 'rectanglelabels':
-    #             print(f"Label: {annotation['value']}")
-    #             print(f"Coordinates: {annotation['rectanglelabels'][0]['rectangle']}")
-    #         elif annotation['type'] == 'choices':
-    #             print(f"Label: {annotation['value']}")
-    #         elif annotation['type'] == 'keypointlabels':
-    #             print(f"Label: {annotation['value']}")
-    #             print(f"Coordinates: {annotation['keypointlabels'][0]['keypoint']}")
+    if request.method == "POST":
+        data = request.json
+        frame_id = data["task"]["data"]["file_name"]
+        client = connect_postgres()
+        with client.cursor() as cursor:
+            for annotation in data["annotation"]["result"]:
+                if annotation["type"] == "rectanglelabels":
+                    cursor.execute(
+                        "INSERT INTO frame_rectangle_annotation (id, frame_id, class, x, y, width, height) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                        (
+                            uuid.uuid4(),
+                            frame_id,
+                            annotation["value"]["rectanglelabels"][0],
+                            annotation["value"]["x"],
+                            annotation["value"]["y"],
+                            annotation["value"]["width"],
+                            annotation["value"]["height"],
+                        ),
+                    )
+                elif annotation["type"] == "choices":
+                    cursor.execute(
+                        "INSERT INTO frame_classification (id, frame_id, class) VALUES (%s, %s, %s)",
+                        (
+                            uuid.uuid4(),
+                            frame_id,
+                            annotation["value"]["choices"][0],
+                        ),
+                    )
+                elif annotation["type"] == "keypointlabels":
+                    cursor.execute(
+                        "INSERT INTO frame_keypoint_annotation (id, frame_id, class, x, y) VALUES (%s, %s, %s, %s, %s)",
+                        (
+                            uuid.uuid4(),
+                            frame_id,
+                            annotation["value"]["keypointlabels"][0],
+                            annotation["value"]["x"],
+                            annotation["value"]["y"],
+                        ),
+                    )
+            client.commit()
+            # # Deduplicate the data
+            # cursor.execute(
+            #     "DELETE FROM frame_rectangle_annotation WHERE id IN (SELECT id FROM frame_rectangle_annotation GROUP BY frame_id, class, x, y, width, height HAVING COUNT(*) > 1)"
+            # )
+            # cursor.execute(
+            #     "DELETE FROM frame_classification WHERE id IN (SELECT id FROM frame_classification GROUP BY frame_id, class HAVING COUNT(*) > 1)"
+            # )
+            # cursor.execute(
+            #     "DELETE FROM frame_keypoint_annotation WHERE id IN (SELECT id FROM frame_keypoint_annotation GROUP BY frame_id, type, x, y HAVING COUNT(*) > 1)"
+            # )
 
-    #     return jsonify({"message": "Success"})
-    return jsonify({"message": "Success"})
-    
+        return jsonify({"message": "Success"})
+
+
 @webhook_service.route("/test", methods=["POST"])
 def test():
     print("Received POST request")
